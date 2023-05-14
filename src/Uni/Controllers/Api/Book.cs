516bcd5c74f11cb2d9ff216b650031568923c07d
Egor Boyarkin <egrsfn@gmail.com>
2023-05-14 03:56:58 +0300

    Add mock booking API

---
+++ src/Uni/Controllers/Api/Book.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Uni.Controllers.api;
+
+[Route("/api/book")]
+[ApiController]
+public class Book : Controller
+{
+    public record BookModel
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = null!;
+        [Required]
+        [MaxLength(16)]
+        public string Phone { get; set; } = null!;
+        [Required]
+        public DateTime Date { get; set; }
+        public string? Message { get; set; }
+        public bool IsMessage => Message == "on";
+    }
+
+    [HttpPost]
+    public IActionResult Post([FromForm] BookModel bookInfo)
+    {
+        return Ok();
+    }
+}
