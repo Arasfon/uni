107638ce03592e56815037f342f274792d020189
Egor Boyarkin <egrsfn@gmail.com>
2023-05-15 20:26:37 +0300

    Decouple navigation script from page script

--- src/Uni/wwwroot/js/index.ts
+++ src/Uni/wwwroot/js/index.ts
@@ -1,75 +1,9 @@
-import anime from "animejs";
 import flatpickr from "flatpickr";
 import { Russian as flatpickrRu } from "flatpickr/dist/l10n/ru.js";
 
-function switchMenuOnOuterClick(e: Event) {
-    const element = e.target;
-    if (element instanceof HTMLElement && (element as HTMLElement).classList.contains("nav-menu__links-wrapper")) {
-        switchMenu();
-    }
-}
+import MobileNavigation from "./navigation";
 
-let menuState = 0;
-
-function switchMenu() {
-    const linksWrapper = document.querySelector(".nav-menu__links-wrapper") as HTMLElement;
-    if (linksWrapper.hasAttribute("open")) {
-        linksWrapper.removeAttribute("open");
-
-        linksWrapper.removeEventListener("click", switchMenuOnOuterClick, true);
-
-        document.body.style.removeProperty("overflow-y");
-
-        menuState = 0;
-
-        anime({
-            targets: linksWrapper,
-            easing: "easeOutQuart",
-            duration: 300,
-            opacity: 0,
-            complete: anim => {
-                if (menuState !== 0)
-                    return;
-
-                linksWrapper.style.removeProperty("opacity");
-                linksWrapper.style.removeProperty("visibility");
-            }
-        });
-    } else {
-        linksWrapper.setAttribute("open", "");
-
-        linksWrapper.style.visibility = "visible";
-        document.body.style.overflowY = "hidden";
-
-        linksWrapper.addEventListener("click", switchMenuOnOuterClick, true);
-
-        menuState = 1;
-
-        anime({
-            targets: linksWrapper,
-            easing: "easeOutQuart",
-            duration: 300,
-            opacity: 1,
-            complete: anim => {
-                if (menuState !== 1)
-                    return;
-
-                linksWrapper.style.visibility = "visible";
-            }
-        });
-
-        anime({
-            targets: ".nav-menu__links-wrapper a",
-            easing: "easeOutQuart",
-            duration: 300,
-            translateX: ["-1rem", "0"],
-            opacity: [0, 1],
-            delay: anime.stagger(50)
-        });
-    }
-}
-
-document.querySelector(".nav-menu__menu-button")!.addEventListener("click", switchMenu);
+const mobileNavigation = new MobileNavigation();
 
 // @ts-ignore
 flatpickr("#datetime",
---
+++ src/Uni/wwwroot/js/navigation.ts
@@ -0,0 +1,74 @@
+import anime from "animejs";
+
+export default class MobileNavigation {
+    private menuState = 0;
+    private outerClickLambda = (e) => this.switchMenuOnOuterClick(e);
+
+    constructor() {
+        document.querySelector(".nav-menu__menu-button")!.addEventListener("click", this.switchMenu.bind(this));
+    }
+
+    private switchMenuOnOuterClick(e: Event) {
+        if (e.target === e.currentTarget) {
+            this.switchMenu();
+        }
+    }
+
+    switchMenu() {
+        const linksWrapper = document.querySelector(".nav-menu__links-wrapper") as HTMLDivElement;
+        if (linksWrapper.hasAttribute("open")) {
+            linksWrapper.removeAttribute("open");
+
+            linksWrapper.removeEventListener("click", this.outerClickLambda);
+
+            document.body.style.removeProperty("overflow-y");
+
+            this.menuState = 0;
+
+            anime({
+                targets: linksWrapper,
+                easing: "easeOutQuart",
+                duration: 300,
+                opacity: 0,
+                complete: () => {
+                    if (this.menuState !== 0)
+                        return;
+
+                    linksWrapper.style.removeProperty("opacity");
+                    linksWrapper.style.removeProperty("visibility");
+                }
+            });
+        } else {
+            linksWrapper.setAttribute("open", "");
+
+            linksWrapper.style.visibility = "visible";
+            document.body.style.overflowY = "hidden";
+
+            linksWrapper.addEventListener("click", this.outerClickLambda);
+
+            this.menuState = 1;
+
+            anime({
+                targets: linksWrapper,
+                easing: "easeOutQuart",
+                duration: 300,
+                opacity: 1,
+                complete: () => {
+                    if (this.menuState !== 1)
+                        return;
+
+                    linksWrapper.style.visibility = "visible";
+                }
+            });
+
+            anime({
+                targets: ".nav-menu__links-wrapper a",
+                easing: "easeOutQuart",
+                duration: 300,
+                translateX: ["-1rem", "0"],
+                opacity: [0, 1],
+                delay: anime.stagger(50)
+            });
+        }
+    }
+}
