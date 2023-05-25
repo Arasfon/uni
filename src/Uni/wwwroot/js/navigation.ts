96864c7a87ca11628ca708ff6c50b19cc9417479
Egor Boyarkin <egrsfn@gmail.com>
2023-05-26 01:20:52 +0300

    Add dropdown menu

--- src/Uni/wwwroot/js/navigation.ts
+++ src/Uni/wwwroot/js/navigation.ts
@@ -5,10 +5,17 @@ export default class MobileNavigation {
     private outerClickLambda = (e) => this.switchMenuOnOuterClick(e);
 
     constructor() {
-        document.querySelector(".nav-menu__menu-button")!.addEventListener("click", this.switchMenu.bind(this));
+        document.querySelector(".nav-menu__menu-button")!.addEventListener("click", () => this.switchMenu());
+        for (let headerSwitch of document.querySelectorAll(".dropdown__header__switch")) {
+            headerSwitch.addEventListener("click", (e) => this.switchDropdown(e, headerSwitch as HTMLElement, ((headerSwitch.parentNode as HTMLElement).parentNode as HTMLElement).querySelector(".dropdown__content") as HTMLElement));
+        }
     }
 
     private switchMenuOnOuterClick(e: Event) {
+        if ((e.target as HTMLElement).classList.contains("dropdown__header__switch") ||
+            ((e.target as HTMLElement).parentElement as HTMLElement).classList.contains("dropdown__header__switch"))
+            return;
+
         this.switchMenu();
     }
 
@@ -34,6 +41,12 @@ export default class MobileNavigation {
 
                     linksWrapper.style.removeProperty("opacity");
                     linksWrapper.style.removeProperty("visibility");
+
+                    const dropdowns = linksWrapper.querySelectorAll(".dropdown");
+                    for (let dropdown of dropdowns) {
+                        (dropdown.querySelector(".dropdown__content") as HTMLElement).removeAttribute("open");
+                        (dropdown.querySelector(".dropdown__header__switch") as HTMLElement).removeAttribute("on");
+                    }
                 }
             });
         } else {
@@ -60,7 +73,28 @@ export default class MobileNavigation {
             });
 
             anime({
-                targets: ".nav-menu__links-wrapper a",
+                targets: ".nav-menu__links-wrapper > *",
+                easing: "easeOutQuart",
+                duration: 300,
+                translateX: ["-1rem", "0"],
+                opacity: [0, 1],
+                delay: anime.stagger(50)
+            });
+        }
+    }
+
+    switchDropdown(e: Event, source: HTMLElement, content: HTMLElement) {
+        e.preventDefault();
+
+        if (content.hasAttribute("open")) {
+            content.removeAttribute("open");
+            source.removeAttribute("on");
+        } else {
+            content.setAttribute("open", "");
+            source.setAttribute("on", "");
+
+            anime({
+                targets: content.children,
                 easing: "easeOutQuart",
                 duration: 300,
                 translateX: ["-1rem", "0"],
